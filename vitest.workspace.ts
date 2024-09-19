8abf268870480c39297ec2f7cfd8918af06d168e
YiJie <yijie4188@gmail.com>
2024-09-19 15:54:22 +0800

    chore: abstract filter files function

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -6,6 +6,8 @@ import { defineWorkspace } from 'vitest/config'
 
 import tsconfig from './tsconfig.json'
 
+const relativeCWD = (p: string) => path.relative(process.cwd(), p)
+
 const { references } = tsconfig
 interface TSConfig {
   include?: string[]
@@ -19,19 +21,14 @@ for (const { path: p } of references) {
   const projectTSConfigStr = fs.readFileSync(p, 'utf-8')
   const relativePath = path.relative(process.cwd(), p)
   const dirPath = path.dirname(relativePath)
+  const filterSpecFiles = (globs: string[] = []) => globs
+    .map(relativeCWD)
+    .filter(i => i.includes('.spec.'))
 
   // TODO support jsonc
   const projectTSConfig = JSON.parse(projectTSConfigStr) as TSConfig
-  projectTSConfig.include = projectTSConfig.include
-    ?.map(i => path.resolve(dirPath, i))
-    ?.map(i => i.replace(new RegExp(`^${process.cwd()}${path.sep}`), ''))
-    ?.filter(i => i.includes('.spec.'))
-    ?? []
-  projectTSConfig.exclude = projectTSConfig.exclude
-    ?.map(i => path.resolve(dirPath, i))
-    ?.map(i => i.replace(new RegExp(`^${process.cwd()}${path.sep}`), ''))
-    ?.filter(i => i.includes('.spec.'))
-    ?? []
+  projectTSConfig.include = filterSpecFiles(projectTSConfig.include?.map(i => path.resolve(dirPath, i)))
+  projectTSConfig.exclude = filterSpecFiles(projectTSConfig.exclude?.map(i => path.resolve(dirPath, i)))
   if (projectTSConfig.include.length !== 0 && projectTSConfig.exclude.length !== 0) {
     referenceProjects.push([p, projectTSConfig])
   }
