c3a4bd3f009b5156782ca21f95c17dae4a3bf8e6
yijie4188 <yijie4188@gmail.com>
2024-09-26 17:13:14 +0800

    feat: add unit util function file to spec dts tsconfig file.

--- packages/utils/tests/index.spec.ts
+++ packages/utils/tests/index.spec.ts
@@ -1,8 +1,10 @@
 import { foo } from '@mono/utils'
 import { describe, expect, it } from 'vitest'
+import { echoFooStr } from './echoFooStr'
 
 describe('mono utils', () => {
   it('foo', () => {
+    echoFooStr()
     expect(foo()).toBe('foo')
   })
 })
