d17c40be93ec07e39bb028aa3b29ec93b9df57d3
YiJie <yijie4188@gmail.com>
2024-11-06 13:56:19 +0800

    chore(tests): disable `no-console` rule in unit test files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,15 @@
 import config from '@antfu/eslint-config'
 
-export default config({
-  stylistic: false
-})
+export default config(
+  {
+    stylistic: false
+  },
+  {
+    files: [
+      './pacakges/*/tests/**/*.{js,ts,tsx}'
+    ],
+    rules: {
+      'no-console': 'off'
+    }
+  }
+)
